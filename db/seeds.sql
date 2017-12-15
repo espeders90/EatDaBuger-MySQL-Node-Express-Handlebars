1f3a631014a2a93873f0b889b62df95857994581
Eddie Pedersen <eddiepedersen90@gmail.com>
2017-12-15 17:40:18 +0000

    db setup

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burger (burger_name, devoured) VALUES ('PlainHamburger', false);
+INSERT INTO burger (burger_name, devoured) VALUES ('CheeseBurger',false);
+INSERT INTO burger (burger_name, devoured) VALUES ('BlueCheeseBurger', false);
